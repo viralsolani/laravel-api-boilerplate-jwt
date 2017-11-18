04b3bdcb7f52d4bce4a33e0ead58932fcae3dd06
Vipul Basapati <basapativipulkumar@gmail.com>
2017-11-18 19:12:50 +0530

    added English Translation file for Api

---
+++ resources/lang/en/api.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+	'messages' => [
+		'registeration' => [
+			'success' => "You have registered successfully. Please check your email for activation!"
+		],
+		'login' => [
+			'success' => "Login Successfull.",
+			'failed'  => "Invalid Credentials! Please try again."
+		],
+		'logout' => [
+			'success' => "Successfully logged out."
+		],
+		'refresh' => [
+			'token' => [
+				'not_provided' => 'Token not provided.'
+			],
+			'status' => 'Ok'
+		]
+	]
+
+];
